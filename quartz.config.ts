e7157c1a1558084d65b6deb148071c566df7bce5
Francesco Franco <ffrancoa97@gmail.com>
2024-06-11 23:06:03 -0500

    Prueba de estilo de bloque de código.

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       typography: {
         header: "Lexend Giga", // 1. Rethink Sans 2. Young Serif 3. DM Serif Display
         body: "Lexend", // 1. DM Sans 2. Aleo
-        code: "Fira Code", // 1. DM Mono 2. Fira Code
+        code: "Red Hat Mono", // 1. DM Mono 2. Fira Code
       },
       colors: {
         lightMode: {
@@ -71,8 +71,8 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "github-light",
-          dark: "github-dark",
+          light: "vitesse-light",
+          dark: "vitesse-dark",
         },
         keepBackground: false,
       }),
