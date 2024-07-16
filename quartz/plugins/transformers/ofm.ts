324a14e78dd0498cf9713d2f681768100bf89687
Francesco Franco <ffrancoa97@gmail.com>
2024-07-16 02:19:54 -0500

    Implementación de orden personalizado para archivos y carpetas.

--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -274,7 +274,8 @@ export const ObsidianFlavoredMarkdown: QuartzTransformerPlugin<Partial<Options> 
                     return {
                       type: "html",
                       data: { hProperties: { transclude: true } },
-                      value: `<blockquote class="transclude" data-url="${url}" data-block="${block}"><a href="${url + anchor
+                      value: `<blockquote class="transclude" data-url="${url}" data-block="${block}" data-embed-alias="${alias}"><a href="${
+                        url + anchor
                         }" class="transclude-inner">Transclude of ${url}${block}</a></blockquote>`,
                     }
                   }
